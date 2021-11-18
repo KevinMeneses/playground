f8636c142a233c49f284d6a8de151d49c3a7e86d
Kevin Meneses <kmenesesp@psl.com.co>
2021-11-17 22:10:42 -0500

    project created

---
+++ app/src/main/java/com/meneses/kevin/playground/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.meneses.kevin.playground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.meneses.kevin.playground.ui.theme.PlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { content() }
+    }
+}
+
+@Composable
+private fun content() {
+    PlaygroundTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    content()
+}
